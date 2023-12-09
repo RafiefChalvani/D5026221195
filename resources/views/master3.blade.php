54fefe317eb21f410d49dbb8d2421874ed084f06
RafiefChalvani <rafief.chalvani08@gmail.com>
2023-12-09 15:16:34 +0700

    Tgl 9 Tugas Pra EAS

--- resources/views/master3.blade.php
+++ resources/views/master3.blade.php
@@ -66,6 +66,6 @@
 <br />
 <hr />
 <footer>
-<p>&copy; <a href="https://www.malasngoding.com">www.malasngoding.com</a>. 2018 - 2019</p>
+<p>&copy; <a href="https://www.malasngoding.com">Rafief Chalvani</a>. 2018 - 2019</p>
 </footer>
 </div>
